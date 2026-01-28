9b11cec057e952ca113f2c38d45dd9fa360c76a4
Jorge Venegas <jorgepvenegas@protonmail.com>
2026-01-28 15:37:28 -0800

    replace hardcoded base url

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,5 @@ import type { AppType } from "@quienatiende/api/routes";
 import { hc } from "hono/client";
 
 
-export const client = hc<AppType>("http://localhost:8787");
+export const client = hc<AppType>(process.env.BASE_URL_API!);
 export type Client = typeof client;
